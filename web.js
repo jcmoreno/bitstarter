1e5a497656706eeeea64a79a31137967417982cc
Juan Moreno <moreno.juan23@gmail.com>
2013-07-12 03:33:58 +0000

    Edited web.js for read index

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var indexMsg = fs.readFileSync(index.html);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexMsg);
 });
 
 var port = process.env.PORT || 5000;
